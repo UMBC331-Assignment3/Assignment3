7c32f2ae7a3e7ce2ac1a58982f231a40b487da0c
ayebear <ayebear@gmail.com>
2014-10-07 18:53:55 -0400

    Added combo function that returns combinations

--- assignment3.hs
+++ assignment3.hs
@@ -10,11 +10,17 @@ import System.Environment
 import System.IO
 import Data.List
 
+-- Returns unique combinations, gotten from class
+combo :: Integer -> [a] -> [[a]]
+combo 0 _ = [[]]
+combo n [] = []
+combo n (x:xs) = (map (x:) $ combo (n-1) xs) ++ combo n xs
+
 main = do
    -- Read command line arguments
    args <- getArgs
-   let students = read (head args) :: Int
-   let groupSize = read (last args) :: Int
+   let students = read (head args) :: Integer
+   let groupSize = read (last args) :: Integer
    
    -- Print command line input (for debugging only)
    putStr "Students: "
@@ -22,27 +28,38 @@ main = do
    putStr "Group size: "
    print groupSize
 
+   -- Calculate all combinations of groups of students
+   let allCombinations = (combo groupSize [1..students])
+   print allCombinations
+
    -- Calculate groups for each assignment.
+   -- Could insert all student IDs into a set, so it would check the entire group before adding it to the assignment.
+
+
+   return ()
    
    --Possible way to do this?
-   --If the possible combos > number of projects
-   if students / groupSize < 8
-      then
-   --1. in a list comprehension, create a 3d list for each student to store who they have already worked with
-         --i is the number of student (student id?) and j starts with the a list of size groupSize but all values are false
-         --these are the corresponding "has this student worked with this other student yet?" feel free to change if wrong.
-         [[studentList i j | j <- take groupSize (cycle false) | i <- [0..(groupSize-1)]]
-   --2. Calcluate all possible combinations (permutation(students, groupSize)??)
-         let permTop = product[1..students]
-         let permBottom = product[1..(students-groupSize)]
-         let combinations = permTop / permBottom
-   --3. create groups for all projects (when deciding who is in which group, compare if that person is in any of the
-   --       previous people's "worked with before" list to see if they can be added to the group).
+   ----If the possible combos > number of projects
+   --if students / groupSize < 8
+   --   then do
+   ----1. in a list comprehension, create a 3d list for each student to store who they have already worked with
+   --      --i is the number of student (student id?) and j starts with the a list of size groupSize but all values are false
+   --      --these are the corresponding "has this student worked with this other student yet?" feel free to change if wrong.
+   --      --[studentList i j | j <- take groupSize (cycle false) | i <- [0..(groupSize-1)]]
+   ----2. Calcluate all possible combinations (permutation(students, groupSize)??)
+   --      let permTop = product[1..students]
+   --      let permBottom = product[1..groupSize]
+   --      let combinations = permTop / permBottom
+   --      print permTop
+   --      print permBottom
+   --      print combinations
+   ----3. create groups for all projects (when deciding who is in which group, compare if that person is in any of the
+   ----       previous people's "worked with before" list to see if they can be added to the group).
    
-      else
-   --else print "not possible", return()
-         putStr "I'm sorry, this combination is not possible."
-         return ()
+   --   else do
+   ----else print "not possible", return()
+   --      putStr "I'm sorry, this combination is not possible."
+   --      return ()
    
    
    {-|
@@ -52,6 +69,3 @@ main = do
          Calculate all possible combinations of groups, then figure out a way to split them between assignments.
             The second part of this may be difficult.
    -}
-   let firstGroup = [1 .. students]
-   print firstGroup
-   return ()
