e1c6a2be8221c86dc3ec21f1040dbeba7b871dec
kswaswa <kswa1@umbc.edu>
2014-10-07 22:03:26 -0400

    Update assignment3.hs

--- assignment3.hs
+++ assignment3.hs
@@ -2,13 +2,14 @@
    Usage:          ./assignment3 students groupSize
    Project:        Assignment 3
    Class:          CMSC 331 Fall 2014
-   Authors:        Eric Hebert, Katie Swanson, John Gordon 
+   Authors:        Eric Hebert, Katie Swanson, 
    Description:    Calculates unique groups of student IDs.
 -}
 
 import System.Environment
 import System.IO
 import Data.List
+import qualified Data.Set as Set
 
 -- Returns unique combinations, gotten from class
 combo :: Integer -> [a] -> [[a]]
@@ -16,51 +17,45 @@ combo 0 _ = [[]]
 combo n [] = []
 combo n (x:xs) = (map (x:) $ combo (n-1) xs) ++ combo n xs
 
+--Returns 1 if was inserted into assignment, 0 if needs to be deleted (not sure how to implement, wrote algorithm out)
+--whatToDo :: Set -> Set -> Integer
+--If exactly one of Set to insert is already in the assignment, try all other groups, if no groups work, return 0
+--whatToDo 
+--If groupSize > 2 and two of Set to insert is already in the assignment *in the same group*, return 0
+--whatToDo
+--Else add into a1
+--whatToDo
+
+
 main = do
    -- Read command line arguments
    args <- getArgs
    let students = read (head args) :: Integer
    let groupSize = read (last args) :: Integer
-   
+
    -- Print command line input (for debugging only)
    putStr "Students: "
    print students
    putStr "Group size: "
    print groupSize
 
-   -- Calculate all combinations of groups of students
-   let allCombinations = (combo groupSize [1..students])
-   print allCombinations
+-- let a1, a2, a3, a4, a5, a6, a7, a8 = Set
+-- let numCombos = length (combo groupSize [1..students])
 
-   -- Calculate groups for each assignment.
-   -- Could insert all student IDs into a set, so it would check the entire group before adding it to the assignment.
-
-
+-- again, I know the algorithm, just difficult to implement in haskell
+-- if ((students % groupSize == 0) and numCombos > 8*(students/groupSize)) then do
+--    1. create set of 8 sets of sets of all combinations
+--    	 let allCombos = (Set.fromList $ combo groupSize [1..students])
+--    2. delete all sets that have 2 ppl that have worked together
+--    	 function where it takes a set and compares each number in it to all of the other numbers in the assignment,
+--    	 if all are not unique and at least one repeat, return false, else return true, 2 versions
+--    	 call this function on allCombos which adds groups to assignments if they belong or returns 0
+--    	 if whatToDo allCombos[0] a1 ... whatToDo allCombos[groupSize-1] a8 == 0 then do delete allCombos[x]
+--    3. end up with 8 sets of assignments with all unique groups
+-- else
+--	putStr "this is not possible"
+   
    return ()
-   
-   --Possible way to do this?
-   ----If the possible combos > number of projects
-   --if students / groupSize < 8
-   --   then do
-   ----1. in a list comprehension, create a 3d list for each student to store who they have already worked with
-   --      --i is the number of student (student id?) and j starts with the a list of size groupSize but all values are false
-   --      --these are the corresponding "has this student worked with this other student yet?" feel free to change if wrong.
-   --      --[studentList i j | j <- take groupSize (cycle false) | i <- [0..(groupSize-1)]]
-   ----2. Calcluate all possible combinations (permutation(students, groupSize)??)
-   --      let permTop = product[1..students]
-   --      let permBottom = product[1..groupSize]
-   --      let combinations = permTop / permBottom
-   --      print permTop
-   --      print permBottom
-   --      print combinations
-   ----3. create groups for all projects (when deciding who is in which group, compare if that person is in any of the
-   ----       previous people's "worked with before" list to see if they can be added to the group).
-   
-   --   else do
-   ----else print "not possible", return()
-   --      putStr "I'm sorry, this combination is not possible."
-   --      return ()
-   
    
    {-|
       There are a few possible algorithms we could use:
